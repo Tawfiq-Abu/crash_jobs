bec43f46921d8cdcce715bc1cd3c390d746c6063
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-02-18 01:02:42 +0000

    props in setup and making snippets

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="post">
+      {{post.title}}
+      {{snippet}}
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+    props:['post'],
+    setup (props){
+        const snippet = computed(()=>{
+            return props.post.body.substring(0,60) + '.....'
+        })
+        return{ snippet}
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,72 +1,29 @@
 <template>
   <div class="home">
    <h1>Home</h1>
-   <input type="text" v-model="search">
-   <div v-for="name in namesMatching" :key="name">{{name}}</div>
-   <p>search term -{{search}}</p>
-   <button @click="handleClick">stop watch</button>
-   <h2>Refs</h2>
-   <p>{{ crashOne.name }} - {{crashOne.age}}</p>
-   <button @click="updateCrashOne">update CrashOne</button>
-
-   <h2>Reactive</h2>
-   <p>{{ crashTwo.name}}-{{crashTwo.age}}</p>
-   <button @click="updateCrashTwo">update CrashTwo</button>
-
+   <PostList :posts="posts"/>
+   
   </div>
 </template>
 
 <script>
+import SinglePost from '../components/SinglePost.vue'
 import { ref,reactive } from '@vue/reactivity'
-import { computed, watch, watchEffect } from '@vue/runtime-core'
+import PostList from '../components/PostList.vue'
 // @ is an alias to /src
 
 
 export default {
+  components: { PostList,SinglePost },
   name: 'Home',
   setup(){
-    const crashOne = ref({name:'tawfiq',age:21})
-    const crashTwo = reactive({name:'Machele',age:25})
-    const search = ref('')
+    const posts = ref([
+      { title:'Welcome to the blog',body:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis quia incidunt similique minus dolor explicabo facilis maxime doloremque quidem vitae.',id:1},
+      { title:'Top 5 css tips',body:'Lorem ipsum',id:2}
 
-    const names = ref(['tawfiq','machele','aania','kudra','saachibu','queenie bash','salamatu'])
+    ])
 
-    // the watch function watches an element to see if it changes the run something
-    const stopWatch = watch(search,()=>{
-      console.log('watch function ran')
-    })
-    // watch effect runs when the setup function runs or initializes
-    const stopEffect = watchEffect(()=>{
-      // it is a bit like watch but we don't have to explicitly declare value we'd be
-      // watching,it just looks for dependencies in the function
-      // a lot of the time we use this to get data from a db
-      console.log('watcheffect run on first instance',search.value)
-    })
-    // const name = computed(()=>{
-    //   // the computed function returns a value which would be what the constant is equal to
-    //   // computed property computes a new value based of of other values
-    //   // we used it in the past to return filtered versions of data
-    //   return 'tawfiq'
-    // })
-    const namesMatching = computed(()=>{
-      return names.value.filter((name)=> name.includes(search.value))
-    })
-
-    const updateCrashOne = () =>{
-      crashOne.value.age = 22
-    }
-
-    const updateCrashTwo = () =>{
-      crashTwo.age = 28
-    }
-
-    const handleClick = ()=>{
-      // to stop functions we invoke them 
-      stopWatch()
-      stopEffect()
-    }
-
-    return { crashOne,updateCrashOne,crashTwo,updateCrashTwo,names,search,namesMatching,handleClick}
+    return { posts}
   }
  
 }
